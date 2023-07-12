836949d6d21fff14fb705587bfe3aa821efb539a
Munshi91 <mimunshi91@gmail.com>
2023-07-12 14:22:41 +0600

    let use

--- script.js
+++ script.js
@@ -4,11 +4,11 @@
 // var second_ver = 'world';
 // document.write(first_ver + second_ver);
 
-var x = 30;
-var y = 40;
-console.log(x + y);
+// var x = 30;
+// var y = 40;
+// console.log(x + y);
 // document.write(x + y);
 
-// let x = 20;
-// let y = 30;
-// console.log(x + y);
+let x = 20;
+let y = 30;
+console.log(x + y);
