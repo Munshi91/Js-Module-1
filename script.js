17390f752ac2368feb8de26555b0bee3333b7bb0
Munshi91 <mimunshi91@gmail.com>
2023-07-12 14:21:08 +0600

    oparetor Use

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // document.write('<h2>Hello World</h2>');
 // alert('Hi Friends,you are Learning javascript code');
-var first_ver = 'Hello';
-var second_ver = 'world';
-document.write(first_ver + second_ver);
+// var first_ver = 'Hello';
+// var second_ver = 'world';
+// document.write(first_ver + second_ver);
 
-// var x = 30;
-// var y = 40;
-// // console.log(x + y);
-// document.write(x + y);
+var x = 30;
+var y = 40;
+// console.log(x + y);
+document.write(x + y);
 
 // let x = 20;
 // let y = 30;
