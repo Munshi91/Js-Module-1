3e1c4e3d27b5c8b62c3a9dc6f7da97e2b3e00cc5
Munshi91 <mimunshi91@gmail.com>
2023-07-12 14:20:04 +0600

    var string

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-document.write('<h2>Hello World</h2>');
-alert('Hi Friends,you are Learning javascript code');
+// document.write('<h2>Hello World</h2>');
+// alert('Hi Friends,you are Learning javascript code');
+var first_ver = 'Hello';
+var second_ver = 'world';
+document.write(first_ver + second_ver);
+
+// var x = 30;
+// var y = 40;
+// // console.log(x + y);
+// document.write(x + y);
+
+// let x = 20;
+// let y = 30;
+// console.log(x + y);
